b1707fb24dc69c8030d4349fdeeceb48f1ca0bd4
JPGlaser <astroglaser@gmail.com>
2021-08-01 19:37:41 -0400

    Adding on if the planet transits its star for inclination limits.

--- src/stableplanets/io.py
+++ src/stableplanets/io.py
@@ -38,7 +38,7 @@ class ExoplanetCatalog:
             search_query = "SELECT pl_name,hostname,sy_snum,sy_pnum,pl_orbper,pl_orbpererr1,pl_orbpererr2,pl_bmassj, \
                             pl_bmassjerr1,pl_bmassjerr2,pl_bmassprov,pl_orbeccen,pl_orbeccenerr1,pl_orbeccenerr2, \
                             st_spectype,st_rad,st_raderr1,st_raderr2,st_mass,st_masserr1, \
-                            st_masserr2,st_age,st_ageerr1,st_ageerr2 \
+                            st_masserr2,st_age,st_ageerr1,st_ageerr2, tran_flag,  \
                             FROM pscomppars "
             results = service.search(search_query)
             table_data = vo.dal.TAPResults.to_table(results)
@@ -88,6 +88,7 @@ class ExoplanetCatalog:
                 planet_params = system_params[planet['pl_name']]
                 planet_params.update({'hostname' : planet['hostname']})
                 planet_params.update({'pl_bmassprov' : planet['pl_bmassprov']})
+                planet_params.update({'tran_flag' : planet['tran_flag']})
                 for base_param_name in ['pl_orbper', 'pl_bmassj', 'pl_orbeccen']: #, 'pl_orbincl']:
                     max_param = planet[base_param_name]+planet[base_param_name+'err1']
                     min_param = planet[base_param_name]+planet[base_param_name+'err2']
